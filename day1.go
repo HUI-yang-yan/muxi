7bce2bab59716d6f88ccd356e6c4e1cbd9cf3ec7
itcast <hello@itcast.cn>
2025-06-23 16:05:30 +0800

    day1.go

--- day1.go
+++ day1.go
@@ -1,7 +1,8 @@
 package main
 
 import (
-    "math"
+	"math"
+	"sort"
 )
 
 // 判断一个数是否是质数
@@ -27,6 +28,43 @@ func Prime(n int) []int {
 	}
 	return primes
 }
-func main() {
-    
+
+func Deduplicate(nums []int) []int{
+	unique := make(map[int]bool)
+	for _, num := range nums {
+		unique[num] = true
+	}
+	result := []int{}
+	for num := range unique {
+		result = append(result, num)
+	}
+	return result
+}
+
+type TreeNode struct {
+    Value int
+    Left  *TreeNode
+    Right *TreeNode
+}
+
+func BuildTree(nums []int, i int) *TreeNode {
+	if i >= len(nums) || nums[i] == -1 {
+		return nil
+	}
+	root := &TreeNode{Value: nums[i]}
+	root.Left = BuildTree(nums, 2*i+1)
+	root.Right = BuildTree(nums, 2*i+2)
+	return root
+}
+
+func merge(nums1 []int, m int, nums2 []int, n int) []int {
+    nums3 := make([]int, m+n)
+    for i := 0; i < m; i++ {
+        nums3[i] = nums1[i]
+    }
+    for i := 0; i < n; i++ {
+        nums3[m+i] = nums2[i]
+    }
+    sort.Ints(nums3)
+    return nums3
 }
